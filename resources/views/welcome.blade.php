e0e8b4f4581228264d10e1bec363a30eda3add74
minnaingkyaw <minnaingmdy436@gmail.com>
2022-03-13 20:47:42 +0630

    simple counter livewire component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+  <title>Laravel</title>
 
-        <!-- Styles -->
-        @livewireStyles
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body>
-      
-      @livewireScripts
-    </body>
+  <!-- Styles -->
+  @livewireStyles
+
+  <style>
+    body {
+      font-family: 'Nunito', sans-serif;
+    }
+
+  </style>
+</head>
+
+<body>
+
+  <livewire:counter />
+
+  @livewireScripts
+</body>
+
 </html>
